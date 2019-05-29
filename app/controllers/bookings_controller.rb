e42eaf00124b550d1189f4414a3117fbaca37536
MICHEL <michelemmanuel@hotmail.com>
2019-05-29 19:32:14 +0200

    calendar beginning

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,12 +6,25 @@ class BookingsController < ApplicationController
     dinnerHour = ['19h00', '19h30', '20h00', '20h30', '21h00']
     Date.today.upto(Date.today + 7).each do |date|
       bookingsLunch = Booking.where(restaurant_id: current_user.restaurant.id).where(date: date, hour: lunchHour)
-      @calendar << [date, 'lunch', bookingsLunch.inject(0) { |sum, booking| sum + booking.number_of_customers}]
-
       bookingsDinner = Booking.where(restaurant_id: current_user.restaurant.id).where(date: date, hour: dinnerHour)
-      @calendar << [date,  'dinner', bookingsDinner.inject(0) { |sum, booking| sum + booking.number_of_customers}]
+      @calendar << [date, bookingsLunch, bookingsDinner]
    end
+
+   # @calendar = []
+   #  lunchHour = ['12h00', '12h30', '13h00', '13h30', '14h00']
+   #  dinnerHour = ['19h00', '19h30', '20h00', '20h30', '21h00']
+   #  Date.today.upto(Date.today + 7).each do |date|
+   #    bookingsLunch = Booking.where(restaurant_id: current_user.restaurant.id).where(date: date, hour: lunchHour)
+   #    @calendar << [date, 'lunch', bookingsLunch.inject(0) { |sum, booking| sum + booking.number_of_customers}]
+
+   #    bookingsDinner = Booking.where(restaurant_id: current_user.restaurant.id).where(date: date, hour: dinnerHour)
+   #    @calendar << [date,  'dinner', bookingsDinner.inject(0) { |sum, booking| sum + booking.number_of_customers}]
+   # end
+
+
  end
+
+
 
   def show
     @bookings = Booking.new
