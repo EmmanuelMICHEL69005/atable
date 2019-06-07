5cd7ce9b835ffc61b548b51f10abf9e3201db4bd
MICHEL <michelemmanuel@hotmail.com>
2019-06-07 09:41:19 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -171,11 +171,103 @@ alex_booking1 = Booking.create!(
   date: Date.today,
   number_of_customers: 8,
   restaurant: restaurant,
-  source: 'La Fourchette',
+  source: 'Site web',
   customer: alex,
   hour: '20h30',
   status: 'Valided'
 )
+
+charly = Customer.create!(
+  first_name: "charly",
+  last_name: "grandpre",
+  phone_number: "0675342561",
+  email: "charly.grandpre@gmail.com",
+  )
+
+charly_booking1 = Booking.create!(
+  date: 1.days.from_now,
+  number_of_customers: 7,
+  restaurant: restaurant,
+  customer: charly,
+  source: 'Site web',
+  hour: '19h30',
+  content: 'Terrasse',
+  status: 'New'
+)
+
+pierre = Customer.create!(
+  first_name: "pierre",
+  last_name: "mazille",
+  phone_number: "0675342561",
+  email: "pierre.mazille@gmail.com",
+  )
+
+pierre_booking1 = Booking.create!(
+  date: 1.days.from_now,
+  number_of_customers: 7,
+  restaurant: restaurant,
+  customer: pierre,
+  source: 'Site web',
+  hour: '20h30',
+  content: 'Terrasse',
+  status: 'New'
+)
+
+florian = Customer.create!(
+  first_name: "florian",
+  last_name: "guerbette",
+  phone_number: "0675342561",
+  email: "florian.guerbette@gmail.com",
+  )
+
+florian_booking1 = Booking.create!(
+  date: 1.days.from_now,
+  number_of_customers: 7,
+  restaurant: restaurant,
+  customer: florian,
+  source: 'Site web',
+  hour: '19h30',
+  content: 'Terrasse',
+  status: 'New'
+)
+
+juliette = Customer.create!(
+  first_name: "juliette",
+  last_name: "boge",
+  phone_number: "0675342561",
+  email: "juliette.boge@gmail.com",
+  )
+
+juliette_booking1 = Booking.create!(
+  date: 1.days.from_now,
+  number_of_customers: 7,
+  restaurant: restaurant,
+  customer: juliette,
+  source: 'Site web',
+  hour: '13h00',
+  content: 'Terrasse',
+  status: 'Valided'
+)
+
+léa = Customer.create!(
+  first_name: "léa",
+  last_name: "gavoille",
+  phone_number: "0675342561",
+  email: "léa.gavoille@gmail.com",
+  )
+
+léa_booking1 = Booking.create!(
+  date: 1.days.from_now,
+  number_of_customers: 7,
+  restaurant: restaurant,
+  customer: léa,
+  source: 'Site web',
+  hour: '13h00',
+  content: 'Terrasse',
+  status: 'Valided'
+)
+
+
 
 User.create!(
   email: 'test@atable.live',
